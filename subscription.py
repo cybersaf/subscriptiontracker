5e4caafb7d5d7a4b260978ab951196c07f34500d
Safiyah Sheikh <safiyah@chainhaus.com>
2023-11-11 21:57:23 -0500

    first commit

---
+++ subscription.py
@@ -0,0 +1,8 @@
+subscriptions = []
+
+addSubName = input("Enter name of subscription ")
+addSubPrice = int(input("Enter price"))
+
+subscriptions.append(addSubName)
+subscriptions.append(addSubPrice)
+print(subscriptions)
